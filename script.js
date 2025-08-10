4d594765175d490cee8b9fb6df873f89eeaa28c1
ToriLiv <oamossquintanilla@gmail.com>
2025-08-10 17:18:51 -0600

    cambios finales

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//variables globales
+//-----------------variables globales---------------------
 const operationSelect = document.getElementById('operation');
 const orderContainer = document.getElementById('order-container');
 const orderSelect = document.getElementById('order');
 const submitBtn = document.querySelector('input[type="submit"]');
 
 
-//orden de la operacion resta de vectores
+//----------orden de la operacion resta de vectores----------
 operationSelect.addEventListener('change', () => {
     const operation = operationSelect.value;
     if (operation == "resta") {
@@ -15,7 +15,7 @@ operationSelect.addEventListener('change', () => {
     }
 });
 
-//=====================funciones============================
+//==============funciones de operaciones aritmeticas=================
 function sumarVectores(ax, ay, bx, by) {
     return { x: ax + bx, y: ay + by };
 }
@@ -32,20 +32,21 @@ function restarVectores(ax, ay, bx, by, order) {
 function productoEscalar(ax, ay, bx, by) {
     return (ax * bx) + (ay * by);
 }
-//-------------------------------------------------------------evento click
+
+//las funciones sumar y restar vectores trabajan con propiedades x,y
+
+//------------------------------------------------------evento click----->
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
-
-    //*********************variables para las operaciones***********************
+    //*********************variables para las operaciones*****************
     const ax = parseFloat(document.getElementById('ax').value.trim());
     const ay = parseFloat(document.getElementById('ay').value.trim());
     const bx = parseFloat(document.getElementById('bx').value.trim());
     const by = parseFloat(document.getElementById('by').value.trim());
     const operations = operationSelect.value;
     const order = orderSelect.value;
-
 
     //--------------------validaciones campos vacios----------------
     if (isNaN(ax) || isNaN(ay) || isNaN(bx) || isNaN(by)) {
@@ -58,8 +59,8 @@ submitBtn.addEventListener('click', (e) => {
         return;
     }
 
-    //result------------------------>
-    let result;
+    //------------------------------result------------------------>
+    let result;   //la variable result funciona como un objeto
 
     switch (operations) {
         case "suma":
